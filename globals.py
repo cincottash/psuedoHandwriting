232dd02be0bfc76b5a1ebacaecc53079ca18d5d0
cincottash <cincotta@udel.edu>
2020-05-03 15:25:24 -0400

    Hella formatting

--- globals.py
+++ globals.py
@@ -1,14 +1,20 @@
 WHITE = (255, 255, 255)
 
-#8.5x11 is 2550 * 3300
 canvasWidth = 1700
 canvasHeight = 2200
 
+#Size of a space
 space = 40
 
+#List of chars which can go below the bottom horizontal line
 offCenterCharList = ["y", "p", "g", "j", "q"]
 
+#Where the x coord starts
 xStart = canvasWidth/6 - 10
+
+#Where the y coord starts
 yStart = canvasHeight/6 - 22
+
+#Distance between horizontal lines
 lineGap = 69
 
--- main.py
+++ main.py
@@ -7,31 +7,37 @@ def main():
 
 	canvas, alphabetDict0, alphabetDict1 = setup()
 
-	sentence = "the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog "
+	documentFile = open("document.txt", "r")
 
-	# while(True):
-	paper = pygame.image.load("assets/paper.png")
-	canvas.blit(paper, (0,0))
-
+	document = documentFile.read()
 	
-	displaySentence(canvas, sentence, alphabetDict0, alphabetDict1)
+	#Our college ruled 8.5x11
+	paper = pygame.image.load("assets/paper.png")
+	
+	#x,y coords are top left of image
+	canvas.blit(paper, (0,0))
+	
+	displayDocument(canvas, document, alphabetDict0, alphabetDict1)
 	
 	pygame.display.update()
 
-	pygame.image.save(canvas, "screenshot.jpeg")
+	pygame.image.save(canvas, "output.jpeg")
 	
 	#Open the 8.5x11 with firefox
-	bashCommand = "firefox screenshot.jpeg"
+	bashCommand = "firefox output.jpeg"
 	
 	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 	output, error = process.communicate()
 	exit(0)
 
-def displaySentence(canvas, sentence, alphabetDict0, alphabetDict1):
+def displayDocument(canvas, document, alphabetDict0, alphabetDict1):
+	#The total space from the start of our current horizontal line
 	collectiveSpacing = 0
+
+	#Current horizontal line
 	currentLine = 0
 
-	for char in list(sentence):
+	for char in list(document):
 			
 		#Pick a random dict to use for this specific char
 		dictNum = random.randint(0, 1)
@@ -46,31 +52,39 @@ def displaySentence(canvas, sentence, alphabetDict0, alphabetDict1):
 
 			#Check if we're about to go over the page
 			if(xStart + collectiveSpacing + letterGap + alphabetDict[char].get_rect().size[0] > canvasWidth):
-				#Move to next line
+				#Move to next horizontal line
 				currentLine += 1
 
-				#Reset spacing
+				#Reset total spacing
 				collectiveSpacing = 0
 
 				#add random margin on new line
 				collectiveSpacing += random.uniform(0, 20)
 
-			#Align to bottom of line and keep even spacing and varying height, also check if our char can go below line
+			#Align to bottom of line and keep varying spacing as well as varying height, also check if our char can go below line (e.g like "j")
 			offCenterChar = 0
 			if(char in offCenterCharList):
 				offCenterChar = 1
-			
 
 			collectiveSpacing += letterGap
+			
 			canvas.blit(alphabetDict[char], (xStart + collectiveSpacing, yStart + offCenterChar*alphabetDict[char].get_rect().size[1]/2 + currentLine * lineGap - alphabetDict[char].get_rect().size[1]  + random.uniform(-7, 7)))
+			
+			#Add a gap to compensate for the image length
 			collectiveSpacing += alphabetDict[char].get_rect().size[0]
+		
 		#It's a space
 		elif(char == " "):
+			#A random sized space
 			collectiveSpacing += space * random.uniform(0.8, 1.5)
-		#TODO: ELSE: PUT "?" CHAR THERE OR SOMETHING
+		
+		#If char not in our dict, put unknown char symbol
 		else:
 			unknownChar = pygame.image.load("assets/unknownChar.png")
+			
 			canvas.blit(unknownChar, (xStart + collectiveSpacing, yStart + currentLine * lineGap - unknownChar.get_rect().size[1]))
+			
+			#Add a gap to compensate for the image length
 			collectiveSpacing += unknownChar.get_rect().size[0]
 
 
